995f5ea194f0410e8ffb59bcbe5753602d590817
sitecoremaster <5227854+sitecoremaster@users.noreply.github.com>
2019-12-24 05:19:14 -0800

    Additional Changes for Serialization and IOC

---
+++ src/Foundation/GoogleTagManager/code/RegisterDependencies.cs
@@ -0,0 +1,18 @@
+﻿using Ignium.Foundation.GoogleTagManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ignium.Foundation.GoogleTagManager
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<ITrackerService, TrackerService>();
+        }
+    }
+}
